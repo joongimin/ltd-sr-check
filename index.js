9601802d4f84cf31dfd2857ba6631da84c4fd391
Joon Min <joongi.min@gmail.com>
2022-02-04 09:44:19 -0500

    FEATURE - index.js supports args

--- index.js
+++ index.js
@@ -4,45 +4,52 @@ const fetchMembers = require('./fetchMembers');
 const _ = require('lodash');
 const { wowItemName } = require('./wow');
 
-(async (command) => {
-  if (command === 'attendance') {
+(async (args) => {
+  if (args[0] === 'update') {
     const instances = ['naxxramas', 'aq40', 'bwl', 'mc'];
     instances.forEach(async (instance) => {
       await updateAttendances(instance);
+      console.log(`Updated attendances for '${instance}'`);
     });
     return;
   }
 
-  const softresId = command.replace('https://softres.it/raid/', '');
+  if (args[0] === 'check') {
+    const softresId = args[1].replace('https://softres.it/raid/', '');
 
-  const {
-    softresData,
-    members,
-    invalidReserves,
-    firstReportDate,
-    lastReportDate,
-  } = await checkSoftres(softresId);
-  if (invalidReserves.length) {
-    const { instance } = softresData;
-    console.log(`Invalid reserves for ${instance}`);
-    invalidReserves.forEach(({ name, items, priorityItems }) => {
-      const attendance = members[name] || 0;
+    try {
+      const {
+        softresData,
+        members,
+        invalidReserves,
+        firstReportDate,
+        lastReportDate,
+      } = await checkSoftres(softresId);
+      if (invalidReserves.length) {
+        const { instance } = softresData;
+        console.log(`Invalid reserves for ${instance}`);
+        invalidReserves.forEach(({ name, items, priorityItems }) => {
+          const attendance = members[name] || 0;
+          console.log(
+            `${_.capitalize(name)}(${attendance}) - ${items
+              .map(
+                (i) =>
+                  `${wowItemName(i)}${
+                    priorityItems.includes(i) ? '(Priority)' : ''
+                  }`
+              )
+              .join(', ')}`
+          );
+        });
+      } else console.log(`Everything is valid for ${softresData.instance}`);
       console.log(
-        `${_.capitalize(name)}(${attendance}) - ${items
-          .map(
-            (i) =>
-              `${wowItemName(i)}${
-                priorityItems.includes(i) ? '(Priority)' : ''
-              }`
-          )
-          .join(', ')}`
+        `* Analyzed using reports ${firstReportDate} - ${lastReportDate}`
       );
-    });
-  } else console.log(`Everything is valid for ${softresData.instance}`);
-  console.log(
-    `* Analyzed using reports ${firstReportDate} - ${lastReportDate}`
-  );
-})(process.argv[2]);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+})(process.argv.slice(2));
 
 // const fetchAttendances = require('./fetchAttendances');
 
